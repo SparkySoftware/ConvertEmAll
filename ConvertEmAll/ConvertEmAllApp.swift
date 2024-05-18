1a56e33673e3d31cca9af685a428dc560357653c
SparkySoftware <jamie@thesparky.work>
2024-05-17 21:02:28 -0400

    Initial Commit

---
+++ ConvertEmAll/ConvertEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertEmAllApp.swift
+//  ConvertEmAll
+//
+//  Created by Jamie Galloro on 2024-05-17.
+//
+
+import SwiftUI
+
+@main
+struct ConvertEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
